96ca621daa18e86890d97abfed37d7a3fc61e73c
Laezio Picanço <laeziop28@hotmail.com>
2023-02-15 00:48:10 -0300

    master/feat: endpoint login e instalado o jsonwebtoken para gerar token para o usuario logado

---
+++ src/controladores/userLogin/index.js
@@ -0,0 +1,5 @@
+const loginUser = async (req, res) => {};
+
+module.exports = {
+  loginUser,
+};
--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,15 @@ const { updateRegister } = require("./controladores/updateRegister");
 const { deleteRegister } = require("./controladores/deleteRegister");
 const { urlInvalid } = require("./middlewares/urlInvalid");
 const { registerUser } = require("./controladores/registerUser");
+const { loginUser } = require("./controladores/userLogin");
 const rotas = express();
 
 rotas.post("/cadastrar-usuario", registerUser);
+rotas.post("/login", loginUser);
 
 rotas.get("/home", getDados);
 
-rotas.post("/cadastrar-url", urlInvalid, registerUrl);
+rotas.post("/adicionar-url", urlInvalid, registerUrl);
 
 rotas.put("/home/:id", urlInvalid, updateRegister);
 
