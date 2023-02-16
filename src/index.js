37dc035e4fffab5de2e5cdb9ef30c8dd16e02216
Laezio Picanço <laeziop28@hotmail.com>
2023-02-16 18:06:38 -0300

    master

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
-let express = require("express");
-let cors = require("cors");
-let routes = require("./routes");
-let app = express();
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
+const app = express();
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
