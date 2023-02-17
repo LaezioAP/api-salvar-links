b1a8fb0929492b7b76f984cc3489916a07196112
Laezio Picanço <laeziop28@hotmail.com>
2023-02-16 21:42:50 -0300

    feat/master: adiconado variavel PORT

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ const express = require("express");
 const cors = require("cors");
 const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(routes);
 
-app.listen(process.env.PORT || 3000);
+app.listen(PORT);
